2c8a9698244ced253ff10b147163db3d24242ccf
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-12-26 14:11:15 +0000

    Atualizar

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -42,7 +42,7 @@ continue_btn.onclick = () => {
     start_btn_home.classList.remove("hidden");
     quiz_box.classList.add("activeQuiz"); //show quiz box
     //questions = await questionService.getAll();
-    questions = await questionService.findByLevel(1);
+    questions = questionService.findByLevel(1);
     showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
     startTimer(15); //calling startTimer function
