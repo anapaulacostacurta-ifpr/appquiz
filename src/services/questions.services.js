34dfd731566a81702728646284445ce1ac4ec3d6
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-12-26 14:38:11 +0000

    Atualizar

--- src/services/questions.services.js
+++ src/services/questions.services.js
@@ -1,41 +1,13 @@
 // Serviço para interação com o Firestore
 const questionService = {
     findByLevel: level => {
-        return firebase.firestore()
-            .collection("questions")
+        return firebase.firestore().collection("questions")
             .doc(level)
             .get()
             .then(doc => {
                 return doc.data();
             });
     },  
-    getAll: async () => {
-        // Verifica se as perguntas já estão armazenadas no localStorage
-        const storedQuestions = localStorage.getItem("questions");
-
-        if (storedQuestions) {
-            // Carrega perguntas do localStorage se existirem
-            return JSON.parse(storedQuestions);
-        } else {
-            try {
-                // Caso não existam, busca as perguntas do Firestore
-                const questionsRef = firebase.firestore().collection("questions");
-                const snapshot = await questionsRef.get();
-                const questions = snapshot.docs.map(doc => ({
-                    uid: doc.id,
-                    ...doc.data()
-                }));
-
-                // Armazena as perguntas no localStorage para acessos futuros
-                localStorage.setItem("questions", JSON.stringify(questions));
-                return questions;
-            } catch (error) {
-                console.error("Erro ao carregar perguntas:", error);
-                alert("Falha ao carregar perguntas. Tente novamente mais tarde.");
-                return [];
-            }
-        }
-    },
     findByUid: uid => {
         return firebase.firestore()
             .collection("questions")
